bc308df01823619aa2ff053ba46157b7e945ef60
p1nwin0h <troncoso.alex.94@gmail.com>
2019-08-12 01:31:53 -0400

    first commit spring project

---
+++ src/main/java/com/alexninospa/edificiospring/EdificiospringApplication.java
@@ -0,0 +1,13 @@
+package com.alexninospa.edificiospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdificiospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdificiospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexninospa/edificiospring/EdificiospringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alexninospa.edificiospring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EdificiospringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
